c5043a77867334ea016847312f3cf7f2cf8e08b0
jeetendra.achtani <jeetendra@eliteinfoworld.com>
2018-01-23 11:28:05 +0530

    recyclerview and adapter class on same activity

--- app/src/main/java/com/example/jeetendraachtani/apicallingdemo/ListActivity.java
+++ app/src/main/java/com/example/jeetendraachtani/apicallingdemo/ListActivity.java
@@ -3,14 +3,18 @@ package com.example.jeetendraachtani.apicallingdemo;
 
 
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -96,4 +100,63 @@ public class ListActivity extends AppCompatActivity {
                 }
             });
         }
+
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+        Context ctx;
+        public ArrayList<SurveyModel> data;
+        public RecyclerAdapter(Context ctx, ArrayList<SurveyModel> temdata) {
+            this.ctx=ctx;
+            this.data=temdata;
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            LayoutInflater inflater = LayoutInflater.from(ctx);
+            View view=inflater.inflate(R.layout.list_item,null);
+
+
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            SurveyModel surveyModel;
+
+            SurveyModel product= data.get(position);
+            holder.question_order.setText("question_order ="+product.question_order);
+            holder.options.setText("Options ="+product.options);
+            holder.question_id.setText("Question_Id = "+product.question_id);
+            holder.survey_title.setText("Survey_Title ="+product.survey_title);
+            holder.survey_id.setText("Survey_ID ="+product.survey_id);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView survey_id,question,options,question_id,survey_title,question_order;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                survey_id=itemView.findViewById(R.id.tv_survey_id);
+                options=itemView.findViewById(R.id.tv_options);
+                question=itemView.findViewById(R.id.tv_question);
+                question_id=itemView.findViewById(R.id.tv_question_id);
+                survey_title=itemView.findViewById(R.id.tv_survey_title);
+                question_order=itemView.findViewById(R.id.tv_question_order);
+            }
+
+
+        }
     }
+
+}
+
