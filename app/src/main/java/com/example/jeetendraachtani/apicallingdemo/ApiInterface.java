bc3a9f9814fa3f664a29f762fd8c886403f29fdd
jeetendra.achtani <jeetendra@eliteinfoworld.com>
2018-02-22 18:07:01 +0530

    added new project(Wolfkeeper) api calling

--- app/src/main/java/com/example/jeetendraachtani/apicallingdemo/ApiInterface.java
+++ app/src/main/java/com/example/jeetendraachtani/apicallingdemo/ApiInterface.java
@@ -7,6 +7,7 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by jeetendra.achtani on 22-01-2018.
@@ -29,4 +30,29 @@ public interface ApiInterface {
             @Field("access_token") String access_token,
             @Field("survey_id") String survey_id
     );
+
+
+    //op=login
+    // &email=adarsh@eliteinfoworld.com
+    // &password=admin@123
+    // &platform=1&
+    // iostoken=asdasdasda
+    // &androidtoken
+    // &appmode=2
+
+
+    @POST("ws.wolfkeeper.php?")
+    Call<SignUpResponce> setLogin(
+            @Query("op") String op,
+            @Query("email") String email,
+            @Query("password") String password
+          /*  @Query("platform") String platform,
+            @Query("iostoken") String iostoken,
+            @Query("androidtoken") String androidtoken,
+            @Query("appmode") String appmode,
+            @Query("timezone") String timezone*/
+    );
+
+
+
 }
